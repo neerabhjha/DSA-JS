d4a2e27474dde545f6dc86fcb3468501b4964815
neerabhjha <jhaneerabh@gmail.com>
2025-02-08 16:51:49 +0530

    check prime, prime factors and all divisors added

--- DSA_BASICS/MATHEMATICS/index.js
+++ DSA_BASICS/MATHEMATICS/index.js
@@ -111,28 +111,163 @@ class Solution {
 
     //LCM OF TWO NUMBERS
     //TC: O(lcm - max(a,b))
-    findLCM(a, b){
-        let lcm = Math.max(a,b);
-        while(true){
-            if(lcm % a == 0 && lcm % b == 0) return lcm;
+    findLCM(a, b) {
+        let lcm = Math.max(a, b);
+        while (true) {
+            if (lcm % a == 0 && lcm % b == 0) return lcm;
             lcm++;
         }
     }
 
     //LCM OF TWO NUMBERS USING GCD 
     //TC: O(log min(a,b))
-    findLcmBetterApproach(a,b){
-        return (a * b) / solution.findHCFUsingEucliden(a,b)
+    findLcmBetterApproach(a, b) {
+        return (a * b) / solution.findHCFUsingEucliden(a, b)
     }
 
     //CHECK IF NUMBER IS PRIME OR NOT(naive method)
     //TC: O(n); SC:O(1)
-    checkPrime(n){
-        if(n == 1) return false;
-        for(let i = 2; i <= n - 1; i++){
-            if(n % i == 0) return false
+    checkPrime(n) {
+        if (n == 1) return false;
+        for (let i = 2; i <= n - 1; i++) {
+            if (n % i == 0) return false
         }
         return true
+    }
+
+    //CHECK IF NUMBER IS PRIME OR NOT(sqrt method)
+    //TC: O(sqrt(n)); SC:O(1)
+    checkPrimeUsingSqrt(n) {
+        if (n == 1) return false;
+        for (let i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false
+        }
+        return true
+    }
+
+    //CHECK IF NUMBER IS PRIME OR NOT efficient approach
+    //TC: O(sqrt(n)); SC:O(1) // 3x faster than above approach
+    checkPrimeEfficientApproach(n) {
+        if (n == 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (let i = 5; i * i <= n; i = i + 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false
+        }
+        return true
+    }
+
+    //prime factors 
+    //TC: O(n^3/2 logn)
+    findPrimeFactors(n) {
+        let primeFactors = []
+        for (let i = 2; i < n; i++) {
+            if (solution.checkPrimeEfficientApproach(i)) {
+                let x = i;
+                while (n % x == 0) {
+                    primeFactors.push(i);
+                    x = x * i;
+                }
+            }
+        }
+        return primeFactors
+    }
+
+    //prime factors method - 2
+    //O(n^1/2 logn)
+    findPrimeFactorsMethod2(n) {
+        let results = []
+        if (n <= 1) return;
+        for (let i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                results.push(i);
+                n = n / i;
+            }
+        }
+        if (n > 1) results.push(n)
+        return results
+    }
+
+    //prime factors more efficient solution
+    //TC
+    findPrimeFactorsEfficient(n) {
+        if (n <= 1) return n;
+        let results = []
+        while (n % 2 == 0) {
+            results.push(2);
+            n = n / 2;
+        }
+        while (n % 3 == 0) {
+            results.push(3);
+            n = n / 3;
+        }
+        for (let i = 5; i <= Math.sqrt(n); i += 6) {
+            while (n % i == 0) {
+                results.push(i);
+                n = n / i;
+            }
+            while (n % (i + 2) == 0) {
+                results.push((i + 2));
+                n = n / (i + 2);
+            }
+        }
+        if (n > 3) results.push(n)
+        return results
+    }
+
+    //all divisors of a number
+    //TC : theta(n), sc:theta(1)
+    allDivisors(n) {
+        let results = []
+        for (let i = 1; i <= n; i++) {
+            n % i == 0 ? results.push(i) : null
+        }
+        return results
+    }
+
+    //all divisors of a number more efficient soln
+    //TC : O(sqrt(n)), sc:O(1)
+    allDivisorsEfficient(n) {
+        let results = [];
+        for (let i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) {
+                if (n / i === i) {
+                    results.push(i)
+                } else {
+                    results.push(i)
+                    results.push(n / i)
+                }
+            }
+        }
+        return results
+    }
+
+    //all divisors of a number more efficient soln
+    //TC : O(sqrt(n)), sc:O(1)
+    allDivisorsIncreasingOrder(n) {
+        let i = 1, results = []
+        for (i = 1; i * i < n; i++) {
+            if (n % i == 0) {
+                results.push(i)
+            }
+        }
+
+        for (; i >= 1; i--) {
+            if (n % i == 0) {
+                results.push(n / i)
+            }
+        }
+        return results
+    }
+
+    //all prime numbers less than a number
+    //TC: O(N* sqrt(N))
+    findPrimesLessthanN(n){
+        let results = []
+        for (let index = 2; index <= n; index++) {
+            if(solution.checkPrimeEfficientApproach(index)) results.push(index);
+        }
+        return results
     }
 
 }
@@ -150,6 +285,16 @@ console.log(`Count of trailing 0s in 100 is ${solution.findTrailingZeros(100)}`)
 console.log(`HCF of two numbers is ${solution.findHCF(100, 200)}`);
 console.log(`HCF of two numbers using EUCLIDEN ALGO is ${solution.findHCF(2, 4)}`);
 console.log(`HCF of two numbers using EUCLIDEN ALGO (METHOD-2) is ${solution.findHCF(15, 45)}`);
-console.log(`HCF of two numbers is :: `,solution.gcd(15, 45));
-console.log(`LCM of two numbers is :: `,solution.findLCM(15, 45));
-console.log(`LCM of two numbers using better approach is :: `,solution.findLcmBetterApproach(2, 8));
+console.log(`HCF of two numbers is :: `, solution.gcd(15, 45));
+console.log(`LCM of two numbers is :: `, solution.findLCM(15, 45));
+console.log(`LCM of two numbers using better approach is :: `, solution.findLcmBetterApproach(2, 8));
+console.log(`Check prime or not is :: `, solution.checkPrime(47));
+console.log(`Check prime or not using sqrt is :: `, solution.checkPrimeUsingSqrt(67777777777));
+console.log(`Check prime or not efficient approach :: `, solution.checkPrimeEfficientApproach(67777777777));
+console.log(`Prime factors :: `, solution.findPrimeFactors(12));
+console.log(`Prime factors method 2 :: `, solution.findPrimeFactorsMethod2(28));
+console.log(`Prime factors most efficient solution :: `, solution.findPrimeFactorsEfficient(28));
+console.log(`All divisors method 1 :: `, solution.allDivisors(28));
+console.log(`All divisors method 2 :: `, solution.allDivisorsEfficient(28));
+console.log(`All divisors method 3 :: `, solution.allDivisorsIncreasingOrder(28));
+console.log(`findPrimesLessthanN:`, solution.findPrimesLessthanN(10))
